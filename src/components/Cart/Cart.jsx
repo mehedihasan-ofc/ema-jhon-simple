dd0b56d686066a8b176e6286c2f774087b39d22c
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-03-28 11:00:17 +0600

    Show dara from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,13 +3,20 @@ import './Cart.css';
 
 const Cart = ({cart}) => {
 
-    console.log(cart);
-
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart) {
-        totalPrice = totalPrice + product.price;
+
+/*         if(product.quantity === 0) {
+            product.quantity = 1;
+        }
+
+        product.quantity = product.quantity || 1; */
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     // calculate tax
@@ -24,7 +31,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(0)}</p>
